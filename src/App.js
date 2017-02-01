fb655f60e924a10e1b61faae8e2e2fecd66938e9
David Hayes <drhayes@gmail.com>
2017-01-31 19:22:13 -0600

    Add styled-components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Cats = styled.div`
+  background-color: steelblue;
+  width: 400px;
+  height: 300px;
+`;
 
 class App extends Component {
   render() {
@@ -13,6 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Cats />
       </div>
     );
   }
